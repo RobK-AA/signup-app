0a1c8a7ee3da8fa98924066e3a41d4929d2a4b2a
Robert Kornblum <robkornblum44@gmail.com>
2021-01-14 12:13:47 -0500

    test user query on frontend

---
+++ src/components/User.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const USER_QUERY = gql`
+  {
+    users {
+      id
+      name
+      username
+      email
+      picture
+    }
+  }
+`;
+
+const User = (props) => {
+  const { data } = useQuery(USER_QUERY);
+  return (
+    <div>
+      {data && (
+        <>
+          {data.users.map((user) => {
+            return (
+                <div>
+                    {user.name}, {user.username}, {user.email}, {user.picture}
+                </div>
+                )
+            })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default User;
