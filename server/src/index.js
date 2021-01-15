58c1f7315899bb2a93f2c26c80809b0295bb18da
Robert Kornblum <robkornblum44@gmail.com>
2021-01-14 21:49:30 -0500

    add email and password validations and start authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client');
+const Mutation = require('./resolvers/Mutation');
 const fs = require('fs');
 const path = require('path');
 
@@ -10,33 +11,20 @@ const resolvers = {
             return context.prisma.user.findMany();
         },
     },
-    Mutation: {
-        post: (parent, args, context) => {
-            const newUser = context.prisma.user.create({
-                data: {
-                    username: args.username,
-                    name: args.name,
-                    email: args.email,
-                    password: args.password,
-                    picture: args.picture
-                },
-            })
-            return newUser
-          },
-      },
+    Mutation
 }
 
 const prisma = new PrismaClient()
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
-  resolvers,
-  context: {
-    prisma,
-  }
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    resolvers,
+    context: {
+        prisma,
+    }
 })
 
 server
---
+++ server/src/resolvers/Mutation.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET } = require('../utils');
+
+async function signUp(parent, args, context, info) {
+    if (args.password.length < 6 ) {
+        throw new Error("password should be minimum 6 characters");
+    };
+    const password = await bcrypt.hash(args.password, 10);
+    
+    const emailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isValidEmail =  emailExpression.test(String(args.email).toLowerCase());
+    if (!isValidEmail) {
+        throw new Error("email not in proper format");
+    };
+    const newUser = context.prisma.user.create({
+        data: {
+            username: args.username,
+            name: args.name,
+            email: args.email,
+            password: password,
+            picture: args.picture
+        },
+    })
+    // const token = jwt.sign({ userId: newUser.id }, APP_SECRET);
+    return newUser;
+}
+
+async function login(parent, args, context, info) {
+    const user = await context.prisma.user.findUnique({
+        where: { email: args.email }
+    });
+    if (!user) {
+        throw new Error('No such user found');
+    }
+
+    const valid = await bcrypt.compare(
+        args.password,
+        user.password
+    );
+    if (!valid) {
+        throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+    return {
+        token,
+        user
+    };
+}
+
+module.exports = {
+  signUp,
+  login
+};
